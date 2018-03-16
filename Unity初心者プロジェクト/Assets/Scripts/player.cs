c6772f29b0f2730fad0c8ab9f5485f181c5d1d6d
PG1-36\amg <nishiki@ridi.co.jp>
2018-03-16 14:19:40 +0900

    Unityちゃんの十字キー移動※滑らかなプレイヤー追従カメラが未達成のため現在メインカメラをUnityちゃんのこどもにしています。

---
+++ Unity初心者プロジェクト/Assets/Scripts/player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //  右キーを押したら右に移動
+        if (Input.GetKey(KeyCode.RightArrow)){
+            transform.position += new Vector3( 1.0f, 0.0f, 0.0f) * Time.deltaTime;
+        }
+        //  左キーを押したら左に移動
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            transform.position += new Vector3( -1.0f, 0.0f, 0.0f) * Time.deltaTime;
+        }
+        //  上キーを押したら前に移動
+        if (Input.GetKey(KeyCode.UpArrow)){
+            transform.position += new Vector3( 0.0f, 0.0f, 1.0f) * Time.deltaTime;
+        }
+        //  下キーを押したら後ろに移動
+        if (Input.GetKey(KeyCode.DownArrow)){
+            transform.position += new Vector3(0.0f, 0.0f, -1.0f) * Time.deltaTime;
+        }
+    }
+}
